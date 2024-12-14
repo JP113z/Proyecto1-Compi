d967e539142bdd8f91e1673ac6c3a9d94db5bbc8
JP113z <josepablof113@gmail.com>
2024-12-14 15:41:52 -0600

    Se crea un archivo .txt con todos los tokens encontrados (Punto B)

--- Proyecto1-Compi/src/Main/MainJFlexCup.java
+++ Proyecto1-Compi/src/Main/MainJFlexCup.java
@@ -8,6 +8,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
 
 public class MainJFlexCup {
 
@@ -36,18 +38,37 @@ public class MainJFlexCup {
             BasicLexerCupV lexer = new BasicLexerCupV(reader);
             Symbol token;
             int i = 0;
-
             System.out.println("Iniciando análisis léxico...");
+            generateFile("", false);
             while ((token = lexer.next_token()).sym != 0) {
-                System.out.printf("Token %d: %s, Valor: %s%n",
-                        token.sym, lexer.yytext(),
-                        token.value == null ? lexer.yytext() : token.value.toString());
+                String action = String.format(
+                        "Token %d: %s, Valor: %s%n",
+                        token.sym,
+                        lexer.yytext(),
+                        token.value == null ? lexer.yytext() : token.value.toString()
+                );
                 i++;
+                System.out.printf(action);
+                generateFile(action, true);
             }
             System.out.println("Análisis léxico completado. Tokens encontrados: " + i);
         } catch (Exception e) {
             System.err.println("Error durante el análisis léxico: " + e.getMessage());
         }
+
+    }
+
+    private void generateFile(String action, boolean replace)
+    {
+        try (
+                FileWriter writer = new FileWriter("tokens_salida.txt", replace)
+        ) {
+            // Escribe en archivo en modo append
+            writer.write(action);
+        } catch (IOException e) {
+            System.err.println("Error escribiendo en el archivo: " + e.getMessage());
+        }
+
     }
 }
 
